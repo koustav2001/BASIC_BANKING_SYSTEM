2580f0a1531e7f3eaf8813a4bb2e09d9692bf8c3
koustav2001 <89209950+koustav2001@users.noreply.github.com>
2021-12-12 12:31:43 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    
+
+	$conn = mysqli_connect('localhost','root','','id29876541_Koustav');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+	
+
+?>
